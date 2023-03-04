77419a8a755ac924212722d3ed281bbe2c0271e7
Kristopher Rahim <kristopherab@gmail.com>
2023-03-04 17:59:51 +0000

    websocket pool for multi clients

--- internal/chat/http/service.go
+++ internal/chat/http/service.go
@@ -2,10 +2,9 @@ package ws
 
 import (
 	"net/http"
+	"redis-ws/pkg/websocket"
 
 	"github.com/go-chi/chi/v5"
-	"github.com/gobwas/ws"
-	"github.com/gobwas/ws/wsutil"
 )
 
 type Service struct {
@@ -26,26 +25,5 @@ func (s *Service) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 func (s *Service) routes() {
 	// connect to websocket here
-	s.mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		conn, _, _, err := ws.UpgradeHTTP(r, w)
-		if err != nil {
-			http.Error(w, "could not upgrade", http.StatusBadRequest)
-			return
-		}
-
-		go func() {
-			defer conn.Close()
-
-			for {
-				p, err := wsutil.ReadClientText(conn)
-				if err != nil {
-					return
-				}
-				err = wsutil.WriteServerText(conn, p)
-				if err != nil {
-					return
-				}
-			}
-		}()
-	})
+	s.mux.HandleFunc("/", websocket.NewClient(0).ServeHTTP)
 }
